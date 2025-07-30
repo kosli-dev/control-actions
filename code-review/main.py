9a34c7c6320874d7f3035768fa399a9ae892a8b3
Tore Martin Hagen <tore@kosli.com>
2025-07-30 11:56:42 +0200

    Added a lot more information in the code-review attestation

--- code-review/main.py
+++ code-review/main.py
@@ -122,6 +122,22 @@ def evaluate_attestation(commit_hash, attestation):
     pr_url = pull_requests[0].get("url", "") if pull_requests else ""
     if pr_url:
         result["pr_url"] = pr_url
+        result["pr_number"] = pr_url.split('/')[-1]
+        result["review_status"] = pull_requests[0].get("state", "")
+        result["pr_author"] = pull_requests[0].get("author", "")
+        result["pr_approvers"] = [approver["username"] for approver in pull_requests[0].get("approvers", [])]
+
+    git_commit_info = attestation.get("git_commit_info", "")
+    if git_commit_info and git_commit_info["sha1"] == commit_hash:
+        result["commit_message"] = git_commit_info.get("message", "")
+        result["commit_timestamp"] = git_commit_info.get("timestamp", "")
+    elif pr_url:
+        for commit in pull_requests[0].get("commits", []):
+            if commit.get("sha1") == commit_hash:
+                result["commit_message"] = commit.get("message", "")
+                result["commit_timestamp"] = commit.get("timestamp", "")
+                break  # stop after finding the match
+
 
     att_type = attestation.get("attestation_type")
     is_compliant = attestation.get("is_compliant", False)
@@ -329,23 +345,23 @@ def main():
     print(json.dumps(output, indent=2))
 
     # Report the code review attestations
-    try:
-        response = report_code_review_attestation(
-            args.kosli_host_name,
-            args.kosli_org,
-            args.kosli_code_review_flow_name,
-            args.kosli_code_review_trail_name,
-            args.kosli_code_review_attestation_type,
-            args.kosli_code_review_attestation_name,
-            args.kosli_api_token,
-            output,
-            "attestations_evidence.json",
-        )
-        print("Code review attestations reported successfully")
-        print(json.dumps(response, indent=2))
-    except Exception as e:
-        print(f"Error reporting code review attestations: {e}", file=sys.stderr)
-        sys.exit(1)
+    # try:
+    #     response = report_code_review_attestation(
+    #         args.kosli_host_name,
+    #         args.kosli_org,
+    #         args.kosli_code_review_flow_name,
+    #         args.kosli_code_review_trail_name,
+    #         args.kosli_code_review_attestation_type,
+    #         args.kosli_code_review_attestation_name,
+    #         args.kosli_api_token,
+    #         output,
+    #         "attestations_evidence.json",
+    #     )
+    #     print("Code review attestations reported successfully")
+    #     print(json.dumps(response, indent=2))
+    # except Exception as e:
+    #     print(f"Error reporting code review attestations: {e}", file=sys.stderr)
+    #     sys.exit(1)
 
 
 if __name__ == "__main__":
